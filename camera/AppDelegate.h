993f694345efccaa339111a8903071ee6f0f0ba2
showmara <showmara@qq.com>
2016-04-12 11:25:46 +0800

    init
    init

---
+++ camera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  camera
+//
+//  Created by s on 16/4/12.
+//  Copyright © 2016年 lanzheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
